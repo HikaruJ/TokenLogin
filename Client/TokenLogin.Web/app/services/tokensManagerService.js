da216194f75608b6c3537fd80fbe0eb96db1dd58
Or <Or@NOGI>
2016-09-10 11:56:40 +0900

    Changing the location of the AuthComplete script

--- Client/TokenLogin.Web/app/services/tokensManagerService.js
+++ Client/TokenLogin.Web/app/services/tokensManagerService.js
@@ -6,14 +6,12 @@
         var tokenManagerServiceFactory = {};
 
         var _getRefreshTokens = function () {
-
             return $http.get(serviceBase + 'api/refreshtokens').then(function (results) {
                 return results;
             });
         };
 
         var _deleteRefreshTokens = function (tokenid) {
-
             return $http.delete(serviceBase + 'api/refreshtokens/?tokenid=' + tokenid).then(function (results) {
                 return results;
             });
